5aff29506ca05d8e6878ddd72968f901fbe3e4a6
mahdi-goleij <161495963+mahdi-goleij@users.noreply.github.com>
2026-07-24 12:03:24 +0330

    update text

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -30,7 +30,7 @@ export const Auth = () => {
         <footer>
           <SvgAuthFooter className='auth-footer-svg'/>
           <div className="auth-footer-inner">
-            <h2>webMG</h2>
+            <h2>webM</h2>
             <SvgLogo />
           </div>
         </footer>
